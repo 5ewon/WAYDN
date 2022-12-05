dc02a2938fcfcc29837f9c4dbde1bf7a84437088
5ewon06 <gwonsewon03@gmail.com>
2022-12-06 04:05:08 +0900

    ADD : add manage page

---
+++ src/components/Manage.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import '../styles/Manage.scss';
+
+const Manage = () => {
+    const [isLogin, setIsLogin] = useState(false);
+    return (
+        <div className="manage-container">
+            <div className="manage-box">
+                <div>ing</div>
+            </div>
+        </div>
+    );
+}
+
+export default Manage;
